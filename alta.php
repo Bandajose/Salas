d6fb93e7cd8602c971660daf5e07fce9c4d41e45
Pablo Ramirez Urcino <pablo.ramirez283@gmail.com>
2017-12-01 04:31:05 +0000

    Solucionando issue #6 y #4

--- alta.php
+++ alta.php
@@ -3,23 +3,24 @@
 require "cfg/conexion.php";
 
 // verificamos si se han enviado ya las variables necesarias.
-if (isset($_GET["nom"])) {
+if (isset($_GET["nom"])) {  
     $name = $_GET["nom"];
     $responsable = $_GET["res"];
     $telefono = $_GET["tel"];
     $email = $_GET["mail"];
     $ip = $_GET["ip"];
     $isdn = $_GET["isdn"];
+    $servicios = $_GET["servicios"];
     
     
     
     // Hay campos en blanco
-    if($name==NULL|$responsable==NULL|$telefono==NULL|$email==NULL|$ip==NULL|$isdn==NULL) {
+    if($name==NULL|$responsable==NULL|$telefono==NULL|$email==NULL|$ip==NULL|$isdn==NULL|$servicios==NULL) {
         echo "un campo est&aacute; vacio.";
         formRegistro();
     }else{
-                
-                $query = 'INSERT INTO SALA_REMOTA (nombre, responsable, telefono, email_responsable, ip, isdn) VALUES ("'.$name.'","'.$responsable.'","'.$telefono.'","'.$email.'","'.$ip.'","'.$isdn.'")';
+
+                $query = 'INSERT INTO SALA_REMOTA (nombre, responsable, telefono, email_responsable, ip, isdn, Servicios) VALUES ("'.$name.'","'.$responsable.'","'.$telefono.'","'.$email.'","'.$ip.'","'.$isdn.'","'.$servicios.'")';
                 mysql_query($query) or die(mysql_error());
                 echo 'La sala '.$name.' ha sido registrada de manera satisfactoria.<br/>';
    header('Location: index.php');
